190a4ca3dc56d9d642967e09feccaf62fdcfb553
Sara Sengi <cooloya@ya.ru>
2021-07-18 17:40:28 +0000

    Intel MAS CLI Tool script

---
+++ SSD_intel.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Select OS to install Intel MAS CLI Tool: "
+options=("CentOS" "Debian/Ubuntu" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "CentOS")
+            rpm -Uvh "http://185.238.170.43:8639/cTKZmhaaRR4/intelmas.rpm" && intelmas show -intelssd && echo -e "See ID of drive and execute this command to update FW\nintelmas load -intelssd <ID>"
+            break
+            ;;
+        "Debian/Ubuntu")
+            apt install wget -y ; wget "http://185.238.170.43:8639/cTKZmhaaRR4/intelmas.deb" && dpkg -i intelmas.deb && rm -f intelmas.deb && intelmas show -intelssd && echo -e "See ID of drive and execute this command to update FW\nintelmas load -intelssd <ID>"
+            break            
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
