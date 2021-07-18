ae944cb75dd240900003d0af2b54bf14e6972f7a
Sara Sengi <cooloya@ya.ru>
2021-07-18 16:51:58 +0000

    Disks Monitoring script added

---
+++ Disk_Mon.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+mondisk='Select OS: '
+options=("CentOS 6/7" "CentOS 8" "Debian/Ubuntu" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "CentOS 6/7")
+            yum install curl -y ; curl "https://raw.githubusercontent.com/sarasengi/cTKZmhaaRR4/master/mon_disk_centos.sh" | sh && echo "Done"
+            break
+            ;;
+        "CentOS 8")
+            yum install curl -y ; curl "https://raw.githubusercontent.com/sarasengi/cTKZmhaaRR4/master/mon_disk_centos8.sh" | sh && echo "Done"
+            break
+            ;;
+        "Debian/Ubuntu")
+            apt install curl -y ; curl "https://raw.githubusercontent.com/sarasengi/cTKZmhaaRR4/master/mon_disk_deb.sh" | sh && echo "Done"
+            break            
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
