ca28982f237566c5dbb12e3770a807723444f7a6
Sara Sengi <cooloya@ya.ru>
2022-06-22 18:19:17 +0000

    Edited SSD_intel_auto

--- SSD_intel_auto.sh
+++ SSD_intel_auto.sh
@@ -4,7 +4,7 @@
 OS=$(cat /etc/os-release | grep "^ID=" | sed 's/ID=//g' | sed 's/"//g')
 
 # If OS is CentOS then install rpm package
-        if [ "${OS}" = "centos" ]; then
+        if [ "${OS}" = "centos" ] || [ "${OS}" = "almalinux" ]; then
 	yum install wget -y
 	rpm -Uvh "http://185.238.170.43:8639/cTKZmhaaRR4/intelmas.rpm" && intelmas show -intelssd && echo -e "See ID of drive with old FW and execute this command to update:\nintelmas load -intelssd <ID>\n\nIf Hrdware RAID installed execute this commands to list drives and update:\nintelmas set -system EnableLSIAdapter='true'\nintelmas show -intelssd\nintelmas load -intelssd <ID>"
         else
