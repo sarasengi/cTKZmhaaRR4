e1f4d1e89a39fd9c4214f6e1e5fdad4afe4c06c7
Sara Sengi <cooloya@ya.ru>
2022-09-15 11:07:25 +0000

    e2fsck updater updated

--- e2fsck_upgrade.sh
+++ e2fsck_upgrade.sh
@@ -1,7 +1,7 @@
 cd /root/
-wget -4 https://mirrors.edge.kernel.org/pub/linux/kernel/people/tytso/e2fsprogs/v1.46.2/e2fsprogs-1.46.2.tar.gz
-tar -xf e2fsprogs-1.46.2.tar.gz
-cd e2fsprogs-1.46.2/
+wget -4 https://mirrors.edge.kernel.org/pub/linux/kernel/people/tytso/e2fsprogs/v1.46.5/e2fsprogs-1.46.5.tar.gz --no-check-certificate
+tar -xf e2fsprogs-1.46.5.tar.gz
+cd e2fsprogs-1.46.5
 ./configure
 make
 make install
